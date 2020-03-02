18576d8e3703c54d1ab506e4875f4d21e1d4126c
Ahmed Ali M. Soliman <ahmed-3010@hotmail.com>
2020-03-02 23:37:07 +0400

    task zero

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -32,7 +32,7 @@ ChatBot::ChatBot(std::string filename)
 
 ChatBot::~ChatBot()
 {
-    std::cout << "ChatBot Destructor" << std::endl;
+    std::cout << "ChatBot Image Destructor" << std::endl;
 
     // deallocate heap memory
     if(_image != NULL) // Attention: wxWidgets used NULL and not nullptr
